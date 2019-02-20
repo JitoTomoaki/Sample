d5796793100e8d42bb24875856613537b67e776f
Jito MacBook <brackstar931000825@icloud.com>
2019-02-20 15:33:54 +0900

    Sample2の変更

--- Sample2.cpp
+++ Sample2.cpp
@@ -7,5 +7,8 @@ int main()
   cout << "Sample12 ブランチ";
   cout << "Sample123 ブランチ";
   cout << "Sample124 ブランチ";
+  cout << "Sample124 ブランチ";
+  cout << "Sample127 ブランチ";
+  cout << "Sample129 ブランチ";
   return 0;
 }
